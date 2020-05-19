cd2c4c35ea3fd1f996cf28ed2ead281ace4c8ec6
ivan chung <ivan0703@gmail.com>
2020-05-20 00:49:49 +0800

    128. Longest Consecutive Sequence

---
+++ 128. Longest Consecutive Sequence/main.py
@@ -0,0 +1,22 @@
+from typing import List
+import math
+
+class Solution:
+    def count(self, key, uset):
+        if key not in uset:
+            return 0
+        else:
+            uset.remove(key)
+            return 1 + self.count(key+1, uset) + self.count(key-1, uset)
+
+    def longestConsecutive(self, nums: List[int]) -> int:
+        uset = set(nums)
+        res = 0
+        while uset:
+            e = list(uset)[0]
+            res = max(res, self.count(e,uset))
+        return res
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.longestConsecutive([100, 4, 200, 1, 3, 2]))
